55db69aa74a32701ff8eb5bc5b331c8ecde536bd
Austin Maddison <Austin.madison10152@gmail.com>
2024-09-21 23:35:50 +0700

    updated premable

--- src/hw1/my_settings.py
+++ src/hw1/my_settings.py
@@ -1,11 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from joblib import Memory
-
-cachedir = 'cache/'
-memory = Memory(cachedir, verbose=0)
-
-# use using @memory.cache to cache the result of a function
 
 plt.rcParams['text.usetex'] = True
 plt.rcParams['text.latex.preamble'] = (r'\usepackage{amsmath}'
