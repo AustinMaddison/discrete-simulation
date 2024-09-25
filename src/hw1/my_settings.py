ecbd02c6d66c36f0764f221650fa49aecfd2d3f1
Austin Maddison <Austin.madison10152@gmail.com>
2024-09-25 12:18:33 +0700

    Finnished HW1

--- src/hw1/my_settings.py
+++ src/hw1/my_settings.py
@@ -1,7 +1,7 @@
+# Libraries and settings for the homework 1
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import cm
-
 
 plt.rcParams['text.usetex'] = True
 plt.rcParams['text.latex.preamble'] = (r'\usepackage{amsmath}'
