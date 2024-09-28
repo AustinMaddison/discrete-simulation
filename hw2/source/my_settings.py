4162bfa7315a86cc0c89a08f72610f0abe53db08
Austin Maddison <Austin.madison10152@gmail.com>
2024-09-28 09:55:47 +0700

    Added some figires for p1

---
+++ hw2/source/my_settings.py
@@ -0,0 +1,10 @@
+# Libraries and settings for the homework 1
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = (r'\usepackage{amsmath}'
+                                       r'\usepackage{arev}'
+                                       r'\usepackage[T1]{fontenc}'
+                                       r'\normalfont')
