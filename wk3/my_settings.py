d5132ae9d17046f1328619147f494ae65c97d781
Austin Maddison <Austin.madison10152@gmail.com>
2024-10-07 16:10:14 +0700

    .

---
+++ wk3/my_settings.py
@@ -0,0 +1,8 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = (r'\usepackage{amsmath}'
+                                       r'\usepackage{arev}'
+                                       r'\usepackage[T1]{fontenc}'
+                                       r'\normalfont')
