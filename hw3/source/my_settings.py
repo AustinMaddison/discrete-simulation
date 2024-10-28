a51d784f6eb7992870f40d6afb643d221006ab82
Austin Maddison <Austin.madison10152@gmail.com>
2024-10-28 08:53:32 +0700

    hw4

---
+++ hw3/source/my_settings.py
@@ -0,0 +1,13 @@
+# Libraries and settings for the homework 1
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+import timeit
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = (r'\usepackage{amsmath}'
+                                       r'\usepackage{arev}'
+                                       r'\usepackage[T1]{fontenc}'
+                                       r'\normalfont')
